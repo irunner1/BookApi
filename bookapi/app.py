d877c1f450295e2647f1de8db5d60392250b7d96
irunner1 <irunner1@users.noreply.github.com>
2023-09-26 18:38:14 +0000

    ✅ Ready to clone and code.

--- bookapi/app.py
+++ bookapi/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+bookapi API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="bookapi",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://bookapi.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "irunner1",
+        "url": "http://bookapi.com/contact/",
+        "email": "irunner1@bookapi.com",
     },
     license_info={
         "name": "The Unlicense",
